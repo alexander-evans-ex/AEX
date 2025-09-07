01074fd45e72607bbaf553e96cfa1fe0451e6ebd
AEX <eddie.pyram@gmail.com>
2025-09-06 23:46:37 -0400

    Convert API function to CommonJS and remove vercel.json for auto-detection

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,9 +1,9 @@
 // For Vercel deployment - this will be used in production
-import Stripe from 'stripe';
+const Stripe = require('stripe');
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
